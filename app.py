085f938db8e6e6bc4274462f3ad881d10e803858
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2024-02-26 22:21:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ def chat():
     messages = request.form.get("prompts", None)
     apiKey = request.form.get("apiKey", None)
     model = request.form.get("model", "gpt-3.5-turbo-0125")
-    temperature = request.form.get("temperature",0.5)
-    max_tokens =  request.form.get("max_tokens",4000)
-# 获取用户输入的API URL
+    temperature = request.form.get("temperature", 0.5)
+    max_tokens =  request.form.get("max_tokens", 4000)
+    # 获取用户输入的API URL
     api_url = request.form.get("api_url", None)
 
     # 如果用户没有输入API URL，则使用配置文件中的默认值
@@ -56,11 +56,11 @@ def chat():
 
     try:
         resp = requests.post(
-            url=api_url+ "/v1/chat/completions",
+            url=api_url + "/v1/chat/completions",
             headers=headers,
             json=data,
             stream=True,
-	    timeout=(60, 60)  # 连接超时时间为60秒，读取超时时间为60秒
+            timeout=(60, 60)  # 连接超时时间为60秒，读取超时时间为60秒
         )
     except requests.exceptions.Timeout:
         return jsonify({"error": {"message": "请求超时，请稍后再试！", "type": "timeout_error", "code": ""}})
@@ -72,18 +72,23 @@ def chat():
             if chunk:
                 streamStr = chunk.decode("utf-8").replace("data: ", "")
                 try:
-                    streamDict = json.loads(streamStr)  # 说明出现返回信息不是正常数据,是接口返回的具体错误信息
+                    streamDict = json.loads(streamStr)
                 except:
-                    errorStr += streamStr.strip()  # 错误流式数据累加
+                    errorStr += streamStr.strip()
                     continue
-                delData = streamDict["choices"][0]
-                if delData["finish_reason"] != None :
-                    break
+
+                # 检查 streamDict 是否包含 "choices" 键
+                if "choices" in streamDict:
+                    delData = streamDict["choices"][0]
+                    if delData["finish_reason"] is not None:
+                        break
+                    else:
+                        if "content" in delData["delta"]:
+                            respStr = delData["delta"]["content"]
+                            yield respStr
                 else:
-                    if "content" in delData["delta"]:
-                        respStr = delData["delta"]["content"]
-                        # print(respStr)
-                        yield respStr
+                    # 处理没有 "choices" 键的情况
+                    errorStr += f"Unexpected data format: {streamDict}"
 
         # 如果出现错误，此时错误信息迭代器已处理完，app_context已经出栈，要返回错误信息，需要将app_context手动入栈
         if errorStr != "":
