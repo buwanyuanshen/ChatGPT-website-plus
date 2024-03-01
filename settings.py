9e7d1128364832b806d4b8e49f181e4df6d5c7d0
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2024-03-01 14:30:14 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,15 +3,20 @@
 SECRET_KEY = "asghnjadfbtywyn"
 
 # openAi api key
-
 API_KEYS = [
-    "",
-    "",
-  
+    "sk-",
+    "sk-",
 
 ]
 
 # openAi 代理 api
 API_URL = "https://api.openai-proxy.com"
 
+# openAi api key1（用于gpt4模型）
+API_KEYS1 = [
+    "sk-",
+    "sk-",
+]
 
+# openAi 代理 api1（用于gpt4模型)
+API_URL1 = "https://api.openai-proxy.com"
